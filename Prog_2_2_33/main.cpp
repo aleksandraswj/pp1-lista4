74d40e0492f6ad11152d98c644c1407e1a84f3d2
aleksandraswj <141584484+aleksandraswj@users.noreply.github.com>
2023-11-12 19:22:32 +0100

    Programs 2.31 2.32 2.33

---
+++ Prog_2_2_33/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+unsigned int suma (unsigned int n1, unsigned int n2=0, unsigned int n3=0, unsigned int n4=0, unsigned int n5=0)
+{
+    return n1+n2+n3+n4+n5;
+}
+
+int main(int argc, char* argv[])
+{
+    unsigned int narray[5];
+    memset(narray, 0, sizeof(narray));
+    for(int i=1; i<argc; i++ )
+    {
+        narray[i-1]=atoi(argv[i]);
+    }
+    cout << suma(narray[0], narray[1], narray[2], narray[3], narray[4]) <<endl;
+    return 0;
+}
